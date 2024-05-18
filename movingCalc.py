324ede1684a542e7ca3030b1220ea6b391514c1c
Maniwar <mberenji@gmail.com>
2024-05-18 15:01:29 -0700

    Update movingCalc.py

--- movingCalc.py
+++ movingCalc.py
@@ -82,21 +82,53 @@ def display_breakdown(new_annual_house_payment, new_total_property_tax, new_tota
 def display_detailed_calculations(current_monthly_house_payment, current_annual_house_payment, current_total_property_tax, current_total_state_income_tax, current_annual_expenses, current_annual_common_expenses,
                                   new_monthly_house_payment, new_annual_house_payment, new_total_property_tax, new_total_state_income_tax, new_common_expenses, new_annual_expenses, current_state_income_tax_rate, spending_increase_percentage, new_state_income_tax_rate, current_monthly_common_expenses, current_salary, new_salary):
     st.subheader('Detailed Calculations')
+
+    # Add custom styles
+    st.markdown(
+        """
+        <style>
+        .calc-box {
+            border: 2px solid #e0e0e0;
+            padding: 10px;
+            border-radius: 10px;
+            margin-bottom: 10px;
+            background-color: #f9f9f9;
+        }
+        .calc-box h4 {
+            color: #333;
+            font-weight: bold;
+        }
+        .calc-box p {
+            color: #555;
+            font-size: 16px;
+        }
+        .fa-icon {
+            font-size: 18px;
+            margin-right: 10px;
+            color: #555;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     detailed_calculations = {
         'Calculation Steps': [
-            f' - Current Annual House Payment: {current_monthly_house_payment} (monthly house payment) * 12 = ${current_annual_house_payment:,.2f}',
-            f' - Current Annual Property Tax: ${current_total_property_tax:,.2f}',
-            f' - Current Annual State Income Tax: ${current_salary:,.2f} (current salary) * {current_state_income_tax_rate / 100:.2f} = ${current_total_state_income_tax:,.2f}',
-            f' - Current Total Annual Expenses: ${current_annual_house_payment:,.2f} (House Payment) + ${current_total_property_tax:,.2f} (Property Tax) + ${current_total_state_income_tax:,.2f} (State Income Tax) + ${current_annual_common_expenses:,.2f} (Common Expenses) = ${current_annual_expenses:,.2f}',
-            f' - New Annual House Payment: {new_monthly_house_payment} (new monthly house payment) * 12 = ${new_annual_house_payment:,.2f}',
-            f' - New Annual Property Tax: ${new_total_property_tax:,.2f}',
-            f' - New Annual State Income Tax: ${new_salary:,.2f} (desired new salary) * {new_state_income_tax_rate / 100:.2f} = ${new_total_state_income_tax:,.2f}',
-            f' - New Annual Common Expenses: ${current_monthly_common_expenses} (current monthly common expenses) * 12 * (1 + {spending_increase_percentage / 100:.2f}) = ${new_common_expenses:,.2f}',
-            f' - New Total Annual Expenses: ${new_annual_house_payment:,.2f} (House Payment) + ${new_total_property_tax:,.2f} (Property Tax) + ${new_total_state_income_tax:,.2f} (State Income Tax) + ${new_common_expenses:,.2f} (Common Expenses) = ${new_annual_expenses:,.2f}'
+            f'<h4><i class="fas fa-home fa-icon"></i>Current Annual House Payment:</h4> <p>{current_monthly_house_payment} (monthly house payment) * 12 = ${current_annual_house_payment:,.2f}</p>',
+            f'<h4><i class="fas fa-landmark fa-icon"></i>Current Annual Property Tax:</h4> <p>${current_total_property_tax:,.2f}</p>',
+            f'<h4><i class="fas fa-file-invoice-dollar fa-icon"></i>Current Annual State Income Tax:</h4> <p>${current_salary:,.2f} (current salary) * {current_state_income_tax_rate / 100:.2f} = ${current_total_state_income_tax:,.2f}</p>',
+            f'<h4><i class="fas fa-calculator fa-icon"></i>Current Total Annual Expenses:</h4> <p>${current_annual_house_payment:,.2f} (House Payment) + ${current_total_property_tax:,.2f} (Property Tax) + ${current_total_state_income_tax:,.2f} (State Income Tax) + ${current_annual_common_expenses:,.2f} (Common Expenses) = ${current_annual_expenses:,.2f}</p>',
+            f'<h4><i class="fas fa-home fa-icon"></i>New Annual House Payment:</h4> <p>{new_monthly_house_payment} (new monthly house payment) * 12 = ${new_annual_house_payment:,.2f}</p>',
+            f'<h4><i class="fas fa-landmark fa-icon"></i>New Annual Property Tax:</h4> <p>${new_total_property_tax:,.2f}</p>',
+            f'<h4><i class="fas fa-file-invoice-dollar fa-icon"></i>New Annual State Income Tax:</h4> <p>${new_salary:,.2f} (desired new salary) * {new_state_income_tax_rate / 100:.2f} = ${new_total_state_income_tax:,.2f}</p>',
+            f'<h4><i class="fas fa-calculator fa-icon"></i>New Annual Common Expenses:</h4> <p>${current_monthly_common_expenses} (current monthly common expenses) * 12 * (1 + {spending_increase_percentage / 100:.2f}) = ${new_common_expenses:,.2f}</p>',
+            f'<h4><i class="fas fa-calculator fa-icon"></i>New Total Annual Expenses:</h4> <p>${new_annual_house_payment:,.2f} (House Payment) + ${new_total_property_tax:,.2f} (Property Tax) + ${new_total_state_income_tax:,.2f} (State Income Tax) + ${new_common_expenses:,.2f} (Common Expenses) = ${new_annual_expenses:,.2f}</p>'
         ]
     }
+
     for calc in detailed_calculations['Calculation Steps']:
-        st.write(calc)
+        st.markdown(f'<div class="calc-box">{calc}</div>', unsafe_allow_html=True)
+
     return detailed_calculations
 
 
